8314fbbe54f361ee519a897821e2bc7222bf9437
Jymi3105 <jymi3105@hotmail.com>
2020-12-03 20:42:25 +0100

    root

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,5 +1,5 @@
 <?php
-define('DNS','mysql:dbname=conciertos;host=https://node29136-env-8799290.es-1.axarnet.cloud');  
+define('DNS','mysql:dbname=conciertos;host=node29136-env-8799290.es-1.axarnet.cloud');  
 //define('DNS','mysql:dbname=conciertos;host=localhost');  
 define('USERNAME','root');  
 define('PASSWORD','YILebq21419');
